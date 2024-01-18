ede44eaf17e92e7d3331f4dcb382ad2bd6576e98
Daryl Lee <daryl@daryllee.com>
2024-01-18 12:11:59 -0700

    Listing 4-9

--- 04-basicFeatures/main.go
+++ 04-basicFeatures/main.go
@@ -8,6 +8,7 @@ import (
 func main() { 
     const price float32 = 275.00 
     const tax float32 = 27.50 
-    fmt.Println(price + tax)
+    const quantity =  2
+    fmt.Println("Total:", quantity * (price + tax))
 }
 
