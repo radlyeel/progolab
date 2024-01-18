f6b3bbd986baa4cc27ce0b23fa3177a3a50b6577
Daryl Lee <daryl@daryllee.com>
2024-01-18 12:21:02 -0700

    Listing 4-12

--- 04-basicFeatures/main.go
+++ 04-basicFeatures/main.go
@@ -6,9 +6,10 @@ import (
 ) 
 
 func main() { 
-    const price, tax float32 = 275, 27.50 
-    const quantity, inStock = 2, true 
-    fmt.Println("Total:", 2 * quantity * (price + tax)) 
-    fmt.Println("In stock: ", inStock)
+    var price float32 = 275.00 
+    var tax float32 = 27.50 
+    fmt.Println(price + tax) 
+    price = 300 
+    fmt.Println(price + tax)
 }
 
